07f634a0fd83d316506820e8f41675c619dc8e71
rafik.mnatsakanyan <rafik.mnatsakanyan@polixis.com>
2024-12-26 18:28:09 +0400

    27-rd lecture

--- src/test/java/reactive/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/reactive/FluxAndMonoGeneratorServiceTest.java
@@ -1,5 +1,7 @@
 package reactive;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
@@ -25,7 +27,7 @@ class FluxAndMonoGeneratorServiceTest {
     void namesFluxMap() {
         //given
 
-        int stringLength=3;
+        int stringLength = 3;
         //when
         var nextFluxMap = fluxAndMonoGeneratorService.namesFluxMap(stringLength);
         //then
@@ -44,6 +46,112 @@ class FluxAndMonoGeneratorServiceTest {
         //then
         StepVerifier.create(nextFluxImmutability)
             .expectNext("alex", "ben", "chloe")
+            .verifyComplete();
+    }
+
+    @Test
+    void namesFluxFlatMap() {
+        //given
+
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFluxFlatMap();
+
+        //then
+        StepVerifier.create(namesFlux)
+            .expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+            .verifyComplete();
+    }
+
+    @Test
+    void namesFluxFlatMapAsync() {
+        //given
+
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFluxFlatMapAsync();
+
+        //then
+        StepVerifier.create(namesFlux)
+            //.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+            .expectNextCount(9)
+            .verifyComplete();
+    }
+
+    @Test
+    void namesFluxConcatMap() {
+
+        //given
+
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFluxConcatMap();
+
+        //then
+        StepVerifier.create(namesFlux)
+            //.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+            .expectNextCount(9)
+            .verifyComplete();
+
+    }
+
+    @Test
+    void namesMonoMap() {
+        //given
+
+        //when
+        var namesMono=fluxAndMonoGeneratorService.namesMonoMap();
+        //then
+        StepVerifier.create(namesMono).expectNext("ALEX").verifyComplete();
+
+    }
+
+    @Test
+    void namesMonoMapFilter() {
+        //given
+
+        //when
+        var namesMono=fluxAndMonoGeneratorService.namesMonoMapFilter();
+
+        //then
+        StepVerifier.create(namesMono).expectNextCount(1).verifyComplete();
+    }
+
+    @Test
+    void namesMonoFlatMap() {
+        //given
+
+        //when
+        var namesMono=fluxAndMonoGeneratorService.namesMonoFlatMap();
+
+        //then
+        StepVerifier.create(namesMono)
+            .expectNext(List.of("A", "L", "E", "X"))
+            .verifyComplete();
+    }
+
+    @Test
+    void namesMonoFlatMapMany() {
+        //given
+        int stringLength=3;
+
+        //when
+        var namesMono=fluxAndMonoGeneratorService.namesMonoFlatMapMany();
+
+        //then
+        StepVerifier.create(namesMono)
+            .expectNext("A", "L", "E", "X")
+            .verifyComplete();
+    }
+
+    @Test
+    void namesFluxTransform() {
+        //given
+
+        int stringLength=3;
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFluxTransform(stringLength);
+
+        //then
+        StepVerifier.create(namesFlux)
+            .expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
             .verifyComplete();
     }
 }
