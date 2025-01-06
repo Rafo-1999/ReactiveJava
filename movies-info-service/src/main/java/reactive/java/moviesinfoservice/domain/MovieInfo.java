61d82a50d91fca443f2f340dd943ba9cd2f1a430
Rafo-1999 <mn.raf.99@mail.ru>
2025-01-06 20:53:24 +0400

    53 lesson

--- movies-info-service/src/main/java/reactive/java/moviesinfoservice/domain/MovieInfo.java
+++ movies-info-service/src/main/java/reactive/java/moviesinfoservice/domain/MovieInfo.java
@@ -4,14 +4,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @Document
 public class MovieInfo {
@@ -21,14 +21,14 @@ public class MovieInfo {
     private String name;
     private Integer year;
     private List<String> cast;
-    private LocalDate releaseDate;
+    private LocalDate release_date;
 
-    public MovieInfo(String movieInfoId, String name, Integer year, List<String> cast, LocalDate releaseDate) {
+    public MovieInfo(String movieInfoId, String name, Integer year, List<String> cast, LocalDate release_date) {
         this.movieInfoId = movieInfoId;
         this.name = name;
         this.year = year;
         this.cast = cast;
-        this.releaseDate = releaseDate;
+        this.release_date = release_date;
     }
 
 }
